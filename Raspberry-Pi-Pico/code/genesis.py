f4a719a3dd704e82ae6b7c91795432939606ddcb
YJ-Arch <yj1516268@outlook.com>
2021-06-09 17:24:40 +0800

    Pico + code

---
+++ Raspberry-Pi-Pico/code/genesis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File: genesis.py
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2021-06-09 16:48:34
+
+Description: LED灯闪烁
+"""
+
+import random
+
+from machine import Pin, Timer
+
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+
+def genesis(timer):
+    led.toggle()
+    print('{} - Hello world!'.format(random.randint(0, 100000000)))
+
+
+timer.init(freq=5, mode=Timer.PERIODIC, callback=genesis)
